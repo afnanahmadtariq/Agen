5c19136155567a35ec1d41bafc04edcbd7a990c3
afnanahmadtariq <116060169+afnanahmadtariq@users.noreply.github.com>
2024-06-03 16:29:18 +0500

    v0.3.3 (build)

--- lib/Save.dart
+++ lib/Save.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:flutter_html_to_pdf/flutter_html_to_pdf.dart';
 
 Future<void> requestStoragePermission() async {
   final status = await Permission.storage.request();
@@ -12,13 +11,4 @@ Future<void> requestStoragePermission() async {
   } else {
     print('Storage permission denied');
   }
-}
-
-Future<File> convertHtmlToPdf(String htmlContent, String targetPath, String targetFileName) async {
-  final generatedPdfFile = await FlutterHtmlToPdf.convertFromHtmlContent(
-    htmlContent,
-    targetPath,
-    targetFileName,
-  );
-  return generatedPdfFile;
 }
